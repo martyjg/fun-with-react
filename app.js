b6f2eb43373196091d232acf5b5572125fb71e6a
martyjg <martinjosephgormley@gmail.com>
2016-01-27 14:23:54 +0000

    Created a bunch of elements for practice

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 console.log("wat up");
 
-var rootElement = React.createElement('div', {})
+var rootElement = 
+React.createElement("div", {},
+  React.createElement("h1", {}, "Contacts"),
+  React.createElement("ul", {},
+    React.createElement("li", {},
+      React.createElement("h2", {}, "Marty Gormley"),
+      React.createElement("a", {href: "mailto:marty@martygormley.com"}, "marty@martygormley.com")
+      ),
+    React.createElement("li", {},
+      React.createElement("h2", {}, "Nota Reelperson"),
+      React.createElement("a", {href: "mailto:blah@blah.com"}, "blah@blah.com")
+      )
+    )
+  );
 
-ReactDOM.render(rootElement, document.getElementById('react-app'))
+ReactDOM.render(rootElement, document.getElementById("react-app"))
