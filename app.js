cd64799bb4d69f49bdea6e4c2949eabb44f36c9c
martyjg <martinjosephgormley@gmail.com>
2016-01-27 18:29:45 +0000

    Used createClass to create a custom React component

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 console.log("wat up");
 
+var ContactItem  = React.createClass( {
+  propTypes: {
+    name: React.PropTypes.string.isRequired,
+  },
+
+  render: function() {
+    return (
+      React.createElement("li", {className: "Contact"},
+        React.createElement("h2", {className: "Contact-name"}, this.props.name)
+      )
+    )
+  },
+});
+
 var rootElement = 
 React.createElement("div", {},
   React.createElement("h1", {}, "Contacts"),
   React.createElement("ul", {},
-    React.createElement("li", {},
-      React.createElement("h2", {}, "Marty Gormley"),
-      React.createElement("a", {href: "mailto:marty@martygormley.com"}, "marty@martygormley.com")
-      ),
-    React.createElement("li", {},
-      React.createElement("h2", {}, "Nota Reelperson"),
-      React.createElement("a", {href: "mailto:blah@blah.com"}, "blah@blah.com")
+    React.createElement(ContactItem, {name: "Marty Gormley"}
+      // React.createElement("h2", {}, "Marty Gormley"),
+      // React.createElement("a", {href: "mailto:marty@martygormley.com"}, "marty@martygormley.com")
       )
+    // React.createElement("li", {},
+    //   React.createElement("h2", {}, "Nota Reelperson"),
+    //   React.createElement("a", {href: "mailto:blah@blah.com"}, "blah@blah.com")
+    //   )
     )
-  );
+);
 
 ReactDOM.render(rootElement, document.getElementById("react-app"))
